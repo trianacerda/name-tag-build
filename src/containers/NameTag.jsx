84cd5c61cef069324e8fc026fc43372bf9ba9ff8
Triana Cerda <trianaRcerda@gmail.com>
2021-11-05 14:56:55 -0700

    added NameTag Container

--- src/containers/NameTag.jsx
+++ src/containers/NameTag.jsx
@@ -1,5 +1,46 @@
 import React, { Component } from 'react';
+import Controls from '../Controls';
+import Display from '../Display';
 
-export default class NameTag extends Component {
+class NameTag extends Component {
+  state = {
+    greeting: '',
+    tagName: '',
+    submitName: '',
+    submitGreeting: '',
+  };
 
+  handleNameChange = (e) => {
+    this.setState({ tagName: e.target.value });
+  };
+
+  handleGreetingChange = (e) => {
+    this.setState({ greeting: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      submitName: this.state.tagName,
+      submitGreeting: this.state.greeting,
+    });
+  };
+
+  render() {
+    const { tagName, greeting, submitName, submitGreeting } = this.state;
+    return (
+      <>
+        <Controls
+          greeting={greeting}
+          onNameChange={this.handleNameChange}
+          tagName={tagName}
+          onGreetingChange={this.handleGreetingChange}
+          onSubmit={this.handleSubmit}
+        />
+        <Display submitName={submitName} submitGreeting={submitGreeting} />
+      </>
+    );
+  }
 }
+
+export default NameTag;
