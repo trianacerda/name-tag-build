db9295699759a4e5cac455bc555887bb2f225fcd
Triana Cerda <trianaRcerda@gmail.com>
2021-11-05 15:05:22 -0700

    added Controls + propTypes

--- src/components/name-tag/Controls.jsx
+++ src/components/name-tag/Controls.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Controls = () => ();
+export default function Controls({
+  greeting,
+  tagName,
+  onNameChange,
+  onGreetingChange,
+  onSubmit,
+}) {
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="greeting">Greeting</label>
+      <input
+        id="greeting"
+        name="greeting"
+        type="text"
+        value={greeting}
+        onChange={onGreetingChange}
+      />
 
-Controls.propTypes = {};
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        value={tagName}
+        onChange={onNameChange}
+      />
+      <button aria-label="btn">Submit</button>
+    </form>
+  );
+}
 
-export default Controls;
+Controls.propTypes = {
+  tagName: PropType.string.isRequired,
+  greeting: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onNameChange: PropTypes.func.isRequired,
+  onGreetingChange: PropTypes.func.isRequired
+}
